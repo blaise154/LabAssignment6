aa1e5d2d71cdfa86ca98b65be8ad9d5e5ce4d242
Blaise <blaisebf2012@gmail.com>
2024-02-23 20:46:03 -0500

    Completed search function

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
-int search(int numbers[], int low, int high, int value) 
+int search(int numbers[], int low, int high, int value)
 {
+    if (low > high) {
 	return -1;
+}
+    int mid = low + (high - low) / 2;
+    if (numbers[mid] == value) {
+        return mid;
+    } else if (numbers[mid] > value) {
+        return search(numbers, low, mid - 1, value);
+    } else {
+        return search(numbers, mid + 1, high, value);
+    }
 }
 
 void printArray(int numbers[], int sz)
@@ -28,7 +38,7 @@ int main(void)
 	FILE* inFile = fopen("input.txt","r");
 
 	fscanf(inFile, " %d\n", &numInputs);
-	
+
 	while (numInputs-- > 0)
 	{
 		fscanf(inFile, " %d\n", &countOfNums);
